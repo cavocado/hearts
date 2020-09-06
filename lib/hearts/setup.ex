4ab6c486536a5b7fe6ba7f9c1f71d68c25d1c124
Alexa Hunleth <alexa@hunleth.com>
2020-09-06 07:55:58 -0400

    Added deck shuffling function and more game setup

--- lib/hearts/setup.ex
+++ lib/hearts/setup.ex
@@ -1,8 +1,22 @@
 defmodule Setup do
 
-  def shuffleAndDeal() do
-    deck = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51]
+  def main() do
+    hands = shuffleAndDeal() |> Enum.map(hands, fn x -> sortCards(x) end)
+    twoClubs = ## find which hand has the 2 of Clubs
+    nextPlayer = ## player after C2
+    followingPlayer = ## player after nextPlayer
+    lastPlayer = ## player after followingPlayer
+    [hands, [[],[],[],[]], [], false, twoClubs, nextPlayer, followingPlayer, lastPlayer]
+  end
 
+  def shuffleAndDeal() do
+    deck = for number <- '23456789TJQKA', suit <- 'CDHS', do: [suit,number]
+    deck |> Enum.shuffle |> Enum.chunk_every(13)
+  end
+
+  def sortCards(hand) do
+    shand = Enum.sort(hand)
+    ## not finished; closer but not quite
   end
 
 end
